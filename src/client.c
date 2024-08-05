5fcdccf394a0ac1830f12667fb38d20dad76dcb4
Pedro <pedropinedocobo@hotmail.com>
2024-08-05 21:39:17 +0200

    no funciona

--- src/client.c
+++ src/client.c
@@ -3,64 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ppinedo- <ppinedo-@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: ppinedo- <ppinedo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 13:08:11 by ppinedo-          #+#    #+#             */
-/*   Updated: 2024/08/05 13:08:12 by ppinedo-         ###   ########.fr       */
+/*   Updated: 2024/08/05 20:51:15 by ppinedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	send_signals(int pid, char *str)
-{
-	int i;
-	int	j;
+void send_signals(int pid, char *str) {
+    int i, j;
 
     i = 0;
-	while (str[i])
-	{
-        j = -1;
-        while (++j < 8)
-        {
-            if (((unsigned char)(str[i] >> (7 - j)) & 1) == 0)
+    while (str[i]) {
+        j = 7;
+        while (j >= 0) {
+            if (((str[i] >> j) & 1) == 0)
                 kill(pid, SIGUSR1);
-            else if (((unsigned char)(str[i] >> (7 - j)) & 1) == 1)
+            else
                 kill(pid, SIGUSR2);
-            usleep(50);
+            usleep(300);  // Incrementado para mayor estabilidad
+            j--;
         }
-    i++;
-	}
+        i++;
+    }
+    // Send end of message signal
     j = 0;
-    while(j++ < 8)
-    {
+    while (j < 8) {
         kill(pid, SIGUSR1);
-        usleep(50);
+        usleep(300);  // Incrementado para mayor estabilidad
+        j++;
     }
 }
 
-int main(int argc, char **argv)
-{
-    char    *message;
-    int     pid;
+int main(int argc, char **argv) {
+    char *message;
+    int pid;
 
-	if (argc == 3)
-	{
-		pid = ft_atoi(argv[1]);
-        if (!pid)
-        {
-            ft_printf("[ERROR]. Wrong pid\n");
+    if (argc == 3) {
+        pid = ft_atoi(argv[1]);
+        if (pid <= 0) {
+            ft_printf("[ERROR]. Wrong PID\n");
             return(0);
         }
         message = argv[2];
-        if (message[0] == 0)
-            return(ft_printf("[ERROR]. No message\n"), 0);
+        if (message[0] == 0) {
+            ft_printf("[ERROR]. No message\n");
+            return(0);
+        }
         send_signals(pid, message);
-    }
-    else
-    {
+    } else {
         ft_printf("[ERROR]. Wrong arguments\n");
-        ft_printf("This is the format: ./client <PID> <MESSAGE>\n");
+        ft_printf("Usage: ./client <PID> <MESSAGE>\n");
     }
     return(0);
 }
+
--- src/server.c
+++ src/server.c
@@ -3,61 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ppinedo- <ppinedo-@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: ppinedo- <ppinedo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 13:09:40 by ppinedo-          #+#    #+#             */
-/*   Updated: 2024/08/05 16:14:28 by ppinedo-         ###   ########.fr       */
+/*   Updated: 2024/08/05 20:49:57 by ppinedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-int g_call;
+void handler(int x) {
+    static unsigned char bits = 0;
+    static int bit_count = 0;
 
-void	print_bits(int x)
-{
-	static unsigned char	bits;
-	int i;
+    if (x == SIGUSR2) {
+        bits = bits << 1;  // Shift bit left
+    } else if (x == SIGUSR1) {
+        bits = (bits << 1) | 1;  // Shift bit left and add 1
+    }
+    bit_count++;
 
-	if (!bits)
-		bits = 0;
-	if (x == SIGUSR2)
-		bits = bits << 1;
-	if (x == SIGUSR1)
-	{
-		bits == bits << 1;
-		bits |= 1;
-	}
-	if ()
-	g_call++;
+    if (bit_count == 8) {  // After receiving 8 bits (1 byte)
+        if (bits == 0) {  // End of message
+            ft_printf("\n");
+        } else {
+            ft_printf("%c", bits);
+        }
+        bit_count = 0;
+        bits = 0;
+    }
 }
 
-void	handler(int a)
-{
-	if (a == SIGUSR1)
-	{
-		ft_printf("Soy el 1\n");
-		// print_bits(1);
-	}
-	else if (a == SIGUSR2)
-	{
-		ft_printf("Soy el 1\n");
-		// print_bits(0);
-	}
-}
+int main(void) {
+    struct sigaction act;
 
-int	main(void)
-{
-	struct sigaction act;
-
-	ft_printf("Welcome to Pedro's server.\n");
-	ft_printf("My PID is %d\n", getpid());
-	act.sa_handler = handler;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0;
-	g_call = 0;
-	sigaction(SIGUSR1, &act, NULL);
-	sigaction(SIGUSR2, &act, NULL);
-	while (1)
-		pause();
+    ft_printf("Welcome to Pedro's server.\n");
+    ft_printf("My PID is %d\n", getpid());
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGUSR1, &act, NULL);
+    sigaction(SIGUSR2, &act, NULL);
+    while (1)
+        pause();
 }
